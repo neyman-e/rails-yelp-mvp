5e78d8cfddc0bf5992bfb91d3becbb585bf29755
Nestor Martinez <nesturron@gmail.com>
2023-05-24 22:09:28 +0200

    Index route, controller and view created

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,18 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
+
+  # index restaurants
+  get 'restaurants', to: 'restaurants#index'
+
+  # new restaurant
+  # get 'restaurants/new', to: 'restaurants#new'
+  # post 'restaurants', to: 'restaurants#create'
+
+  # view restaurant
+  # get 'restaurants/:id', to: 'restaurants#show'
+
+  # new review
+  # get 'restaurants/:id/reviews/new', to: 'reviews#new'
+  # post 'restaurants/:id/reviews', to: 'reviews#create'
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,20 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+puts 'Cleaning database...'
+Restaurant.destroy_all
+
+puts 'Creating restaurants...'
+kikuchan = { name: 'Ki Ku Chan', address: 'Numancia', category: 'japanese' }
+yoshino = { name: 'Yoshino', address: 'Mallorca', category: 'japanese' }
+cal_capo = { name: 'Cal Capo', address: 'Sabadell', category: 'italian' }
+pizza_bona = { name: 'Pizza bona', address: 'Bruc', category: 'italian' }
+calsots = { name: 'Calçots bons', address: 'Querol', category: 'belgian' }
+
+[kikuchan, yoshino, cal_capo, pizza_bona, calsots].each do |attributes|
+  restaurant = Restaurant.create!(attributes)
+  puts "Restaurant #{restaurant.name} created."
+end
+
+puts 'Finished!'
